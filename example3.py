46474912913e4c94b3ce087f6eacffeb3d166102
E-Ozdemir <daviddeveen89@gmail.com>
2021-03-14 20:25:17 +0100

    data types

---
+++ example3.py
@@ -0,0 +1,9 @@
+a = 12
+print(type(a))  # <class 'int'>
+b = '5'
+print(type(b))  # <class 'str'>
+print(a + int(b))
+print(int("5" + "1"))
+print(type(True))  # <class 'bool'>
+print(str("5" + "1"))
+print("5" + "1")
